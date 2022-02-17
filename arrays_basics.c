dd534ff209d83bca90c63adcffaa2fd3db5d62dd
Aryan <97938438+Nikolas-2@users.noreply.github.com>
2022-02-17 10:06:36 +0530

    Add files via upload

---
+++ arrays_basics.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int marks[4];
+    // Array index starts with 0
+    marks[0] = 89;
+    marks[1] = 93;
+    marks[2] = 87;
+    marks[3] = 94;
+    return 0;
+}
